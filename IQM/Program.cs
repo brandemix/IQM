31af6e7f6d62db6f0d884065450ae1befbde53dd
Brandon Allen <bran.mallen@gmail.com>
2019-11-03 09:54:08 -0800

    main program is a test to compare original program output to current output

--- IQM/Program.cs
+++ IQM/Program.cs
@@ -8,23 +8,34 @@ namespace IQM
         static void Main()
         {
             DateTime beforeTime = DateTime.Now;
+            int equalCount = 0;
+            int totalCount = 0;
             
             try
             {
                 DataSet data = new DataSet();
-                using (StreamReader sr = new StreamReader("data.txt"))
+                using (StreamReader input = new StreamReader("data.txt"))
                 {
-                    using(StreamWriter sw = new StreamWriter("output.txt")) 
+                    using(StreamReader comparer = new StreamReader("output_orig.txt")) 
                     {
                         String line;
-                        while ((line = sr.ReadLine()) != null)
+                        while ((line = input.ReadLine()) != null)
                         {
                             data.AddPoint(Convert.ToInt32(line));                        
                             if (data.Count >= 4)
                             {
+                                totalCount++;
                                 double mean = data.GetIQMean();
-                                sw.WriteLine("Index => {0}, Mean => {1:F2}", data.Count, mean);
-                                //Console.WriteLine("Index => {0}, Mean => {1:F2}", data.Count(), mean);
+                                String output = String.Format("Index => {0}, Mean => {1:F2}", data.Count, mean);
+                                String compare;
+                                if ((compare = comparer.ReadLine()) != null) {
+                                    int equal = String.Compare(compare, output);
+                                    if (equal == 0) {
+                                        equalCount++;
+                                    }
+                                } else {
+                                    throw new Exception("Couldn't read comparer");
+                                }
                             }
                         }
                     }
@@ -39,6 +50,7 @@ namespace IQM
             DateTime afterTime = DateTime.Now;
             TimeSpan diff = afterTime - beforeTime;
             Console.WriteLine("Total Milliseconds: {0}", diff.TotalMilliseconds);
+            Console.WriteLine("{0}%", (equalCount / totalCount) * 100);
         }
     }
 }
